de1e77bcfdd60db16c28830a6f0319c9b10de4a7
Dongri Jin <dongriab@gmail.com>
2012-12-06 11:57:19 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Casa::Application.config.secret_token = '4a9b68a4ec62241a2e6031a24f3a9cd50a0f4eee0b2056df7a79b69b79d478800175d14f18bab365adc1aa553433383b7865c1e410036a147814021338743461'
